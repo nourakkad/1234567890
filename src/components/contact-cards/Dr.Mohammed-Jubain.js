bf1c4faae02c67b078fb41c53385b495ccb4ae1c
Muhammad Nour EldinAkkad <nourakkad07@gmail.com>
2025-08-06 05:14:37 +0300

    Initial commit

--- src/components/contact-cards/Dr.Mohammed-Jubain.js
+++ src/components/contact-cards/Dr.Mohammed-Jubain.js
@@ -1,6 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import { getTranslation } from '../../translations';
 
 const DrMohammedJubain = () => {
   const [currentLanguage, setCurrentLanguage] = useState('EN');
